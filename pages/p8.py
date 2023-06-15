41a041bee98473332923b3289734c6f7b28aeb17
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-15 15:09:01 +0200

    Add files via upload

---
+++ pages/p8.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.subheader('Objectifs :')
+st.subheader('Segmentation des images pour des systèmes embarqués dans des véhicules + mise en production sous forme d’Application Web')
+
+st.image('./pictures/p8.png')
