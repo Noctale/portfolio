ba2c54a809242a0d2dc90bab6fa435cd71bfcfc5
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:28:50 +0200

    Delete p9.py

--- pages/p9.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.subheader('Objectifs :')
-st.subheader('Réaliser une application web de recommandation de lecture d’articles en architecture serverless')
-
-st.image('./pictures/p9.png')
-
-st.write('Liens code source (app inactif) : https://github.com/Noctale/p9app.git')
