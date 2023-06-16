0aae130671252d0a47a0dac3bc6c206db08c4e2f
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:28:20 +0200

    Delete p4.py

--- pages/p4.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.subheader('Objectif :')
-st.subheader('Construire un outil de scoring pour évaluer les capacités de remboursements de crédit des clients')
-
-st.image('./pictures/p4.png')
