3367e4ed3d5a309302f341f53429159a9bd5e40c
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:27:17 +0200

    Delete p1.py

--- pages/p1.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.subheader('Objectif :')
-st.subheader('Utiliser le service d’analyse de texte d’Azure pour détecter la langue d’un message texte')
-
-st.image('./pictures/p1.png')
-
