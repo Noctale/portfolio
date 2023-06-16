cc7a629c6954019f0090faa314489992cc5a077c
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:29:17 +0200

    Add files via upload

---
+++ pages/p06 - NLP - Computer Vision.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.subheader('Objectifs :')
+st.subheader('Détecter les sujets d’insatisfactions + Labelliser les photos d’avis clients dans la restauration')
+
+st.image('./pictures/p6.png')
---
+++ pages/p10 - Chatbot - MLOps.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.subheader('Objectifs :')
+st.subheader('Réaliser un premier MVP de Chatbot de réservation de voyage aérien')
+
+st.write('Liens code source (app inactif) : https://github.com/Noctale/p10_chatbot.git')
+st.image('./pictures/p10.png')
+
