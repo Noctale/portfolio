5872b41c2c9a87706c20dfecefe941b02132db85
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:28:31 +0200

    Delete p6.py

--- pages/p6.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.subheader('Objectifs :')
-st.subheader('Détecter les sujets d’insatisfactions + Labelliser les photos d’avis clients dans la restauration')
-
-st.image('./pictures/p6.png')
