7c89df4feb387bd0718f6417d91ce45b44d9bc9b
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:27:43 +0200

    Delete p2.py

--- pages/p2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.subheader('Objectif :')
-st.subheader('Contribuer à une optimisation des tournées d’entretien des arbres de la ville de Paris')
-
-st.image('./pictures/p2.png')
-
