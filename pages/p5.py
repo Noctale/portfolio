1f66da7508a5b502b3019c0835d2550c6a702edb
Noctale <108392658+Noctale@users.noreply.github.com>
2023-06-16 21:28:26 +0200

    Delete p5.py

--- pages/p5.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.subheader('Objectifs :')
-st.subheader('Segmenter les clients d’un site e-commerce + Etude de la stabilité du modèle dans le temps')
-
-st.image('./pictures/p5.png')
